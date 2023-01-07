8a47f5dc58d7c67b5e91e36e28b69b4d449505e3
Kevin Vu <hongkhanhvu94@gmail.com>
2023-01-07 23:52:36 +1100

    add course.java for homework

---
+++ src/main/java/codingmentor/javabackend/k3/Utils/Course.java
@@ -0,0 +1,53 @@
+package codingmentor.javabackend.k3.Utils;
+
+public class Course {
+	private String name;
+	private String code;
+	private String semester;
+	private int section;
+	private double progress;
+	
+	public Course(String name, String code, String semester, int section, double progress) {
+		super();
+		this.name = name;
+		this.code = code;
+		this.semester = semester;
+		this.section = section;
+		this.progress = progress;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	public int getSection() {
+		return section;
+	}
+	public void setSection(int section) {
+		this.section = section;
+	}
+
+	public double getProgress() {
+		return progress;
+	}
+
+	public void setProgress(double progress) {
+		this.progress = progress;
+	}
+	
+	
+}
